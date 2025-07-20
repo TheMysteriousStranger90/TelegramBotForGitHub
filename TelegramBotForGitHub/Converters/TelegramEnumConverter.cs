2992b7c7c56d78ea080e6cc710ec9ce68ff28aa7
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2025-07-20 09:08:55 +0300

    Dev-2: added new functions and converter

---
+++ TelegramBotForGitHub/Converters/TelegramEnumConverter.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace TelegramBotForGitHub.Converters
+{
+    public class TelegramEnumConverter : StringEnumConverter
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+            {
+                string enumText = reader.Value.ToString();
+                
+                if (objectType.Namespace?.StartsWith("Telegram.Bot") == true)
+                {
+                    enumText = ConvertSnakeCaseToPascalCase(enumText);
+                }
+                
+                try
+                {
+                    if (Enum.TryParse(objectType, enumText, true, out var result))
+                    {
+                        return result;
+                    }
+                }
+                catch
+                {
+                }
+            }
+            
+            return base.ReadJson(reader, objectType, existingValue, serializer);
+        }
+        
+        private string ConvertSnakeCaseToPascalCase(string snakeCase)
+        {
+            if (string.IsNullOrEmpty(snakeCase))
+                return snakeCase;
+                
+            var parts = snakeCase.Split('_');
+            var result = "";
+            
+            foreach (var part in parts)
+            {
+                if (!string.IsNullOrEmpty(part))
+                {
+                    result += char.ToUpper(part[0]) + part.Substring(1).ToLower();
+                }
+            }
+            
+            return result;
+        }
+    }
+}
